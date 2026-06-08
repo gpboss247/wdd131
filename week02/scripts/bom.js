40b5323d3913aaa8f5daab62f8d6a9df63530c1a
gpboss247 <oscarvibez247@gmail.com>
2026-06-08 17:49:31 +0100

    bomc change

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -2,26 +2,67 @@ const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('#list');
 
-button.addEventListener('click', function () {
+let chaptersArray = getChapterList() || [];
 
-    if (input.value.trim() !== '') {
+// Display saved chapters when page loads
+chaptersArray.forEach(chapter => {
+    displayList(chapter);
+});
 
-        const li = document.createElement('li');
-        const deleteButton = document.createElement('button');
+button.addEventListener('click', () => {
 
-        li.textContent = input.value;
-        deleteButton.textContent = '❌';
+    if (input.value !== '') {
 
-        li.append(deleteButton);
-        list.append(li);
+        displayList(input.value);
 
-        deleteButton.addEventListener('click', function () {
-            list.removeChild(li);
-            input.focus();
-        });
+        chaptersArray.push(input.value);
+
+        setChapterList();
 
         input.value = '';
+
         input.focus();
     }
-
 });
+
+function displayList(item) {
+
+    let li = document.createElement('li');
+    let deleteButton = document.createElement('button');
+
+    li.textContent = item;
+
+    deleteButton.textContent = '❌';
+
+    deleteButton.classList.add('delete');
+
+    li.append(deleteButton);
+
+    list.append(li);
+
+    deleteButton.addEventListener('click', function () {
+
+        list.removeChild(li);
+
+        deleteChapter(li.textContent);
+
+        input.focus();
+    });
+}
+
+function setChapterList() {
+    localStorage.setItem('myFavBOMList', JSON.stringify(chaptersArray));
+}
+
+function getChapterList() {
+    return JSON.parse(localStorage.getItem('myFavBOMList'));
+}
+
+function deleteChapter(chapter) {
+
+    chapter = chapter.slice(0, chapter.length - 1);
+
+    chaptersArray = chaptersArray.filter(item => item !== chapter);
+
+    setChapterList();
+}
